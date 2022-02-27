ca38e3b79123c11d0611598b90b542939278de07
junepaykim <49562343+junepaykim@users.noreply.github.com>
2022-02-27 20:31:16 +0900

    2022.02.27
    Sunday~ sry

--- math2/1929_Eratostenes.cpp
+++ math2/1929_Eratostenes.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include<math.h>
+#include<string>
 
 using namespace std;
 
@@ -8,8 +9,31 @@ int main() {
 
     cin >> M >> N;
 
-    for (int i=2; i<=sqrt(N); i++) {
-        
+    char * check = new char[N+1];
+    string answer{""};
+    for(int i=0; i<N; i++) {
+        check[i] = 0;
     }
+
+    for (int i=2; i<=sqrt(N); i++) {
+        if(check[i] != 0) {
+            continue;
+        }
+
+        for(int j=2; i*j <=N; j++) {
+            check[i*j]++;
+        }
+    }
+    
+    check[1] = 1;
+
+    for(int i=M; i<=N; i++) {
+        if(check[i] == 0) {
+            answer += to_string(i)+"\n";
+        }
+    }
+    answer = answer.substr(0, answer.length()-1);
+    cout << answer;
+    delete[] check;
     return 0;
 }
